31ed315b5de56d3356e096abb8bda5cdcf42b734
Revathi <brevathi870@gmail.com>
2022-04-01 11:11:55 +0530

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Revathi on 01/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
